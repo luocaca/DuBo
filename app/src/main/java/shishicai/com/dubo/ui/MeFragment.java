c8735212f4c8c0c7f00c4f86cbe3b16c733ff7db
luocaca <471474615@qq.com>
2018-01-04 19:14:42 +0800

    测试下载

--- app/src/main/java/shishicai/com/dubo/ui/MeFragment.java
+++ app/src/main/java/shishicai/com/dubo/ui/MeFragment.java
@@ -1,8 +1,13 @@
 package shishicai.com.dubo.ui;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import shishicai.com.dubo.R;
+import shishicai.com.dubo.WebActivity;
 import shishicai.com.dubo.base.BaseFragment;
 
 /**
@@ -10,8 +15,44 @@ import shishicai.com.dubo.base.BaseFragment;
  */
 
 public class MeFragment extends BaseFragment {
+
+    RecyclerView rvToDoList;
+
     @Override
     protected void initView(View rootView) {
+
+        rootView.findViewById(R.id.load).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WebActivity.start(getActivity());
+            }
+        });
+
+        rvToDoList = rootView.findViewById(R.id.rvToDoList);
+
+
+        rvToDoList.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        rvToDoList.setAdapter(new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+                View view = LayoutInflater.from(getActivity()).inflate(R.layout.item, null);
+                return new RecyclerView.ViewHolder(view) {
+                };
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 100;
+            }
+        });
+
 
     }
 
